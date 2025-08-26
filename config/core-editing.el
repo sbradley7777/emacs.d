e31f19d9b03bf56c557e01f0600c3337f9b5c9c9
Shane Bradley <68629+sbradley7777@users.noreply.github.com>
2025-08-26 13:47:24 -0400

    add more editing options.
    Signed-off-by: Shane Bradley <68629+sbradley7777@users.noreply.github.com>

--- config/core-editing.el
+++ config/core-editing.el
@@ -5,10 +5,26 @@
 (message "Loading core-editing.el...")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Tabs and Spaces Preferences:
+;; Enhanced editing preferences
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(electric-pair-mode 1)                    ; Auto-close parentheses
+(delete-selection-mode 1)                 ; Replace selected text
+(global-auto-revert-mode 1)              ; Auto-reload changed files
+(setq auto-revert-check-vc-info t)        ; Include VC info in auto-revert
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Better indentation
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; - http://www.emacswiki.org/emacs/NoTabs
-(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4
+              standard-indent 4
+              indent-tabs-mode nil)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Show whitespace
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq whitespace-style '(face trailing tabs tab-mark))
+(global-whitespace-mode 1)
 
 (provide 'core-editing)
 (message "core-editing.el loaded successfully.")
