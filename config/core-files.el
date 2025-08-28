48e7f8644c55be469c9cd45d8c4116c0afafc7ae
Shane Bradley <68629+sbradley7777@users.noreply.github.com>
2025-08-28 12:51:31 -0400

    Reformat the .el file for line length 127.
    Signed-off-by: Shane Bradley <68629+sbradley7777@users.noreply.github.com>

--- config/core-files.el
+++ config/core-files.el
@@ -5,27 +5,19 @@
 (message "Loading core-files.el...")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Dont litter my os with autosaves which are files with ~ and backup
-;; files which are files with #. The preferences are from:
-;; http://snarfed.org/gnu_emacs_backup_files
-;;
-;; This link is what I am currently using because does not create
-;; ~/.emacs_archive/auto-saves-list directory
+;; Don't litter OS with autosaves (~) and backup (#) files. Based on:
+;; http://snarfed.org/gnu_emacs_backup_files and
 ;; http://stackoverflow.com/questions/2020941/emacs-newbie-how-can-i-hide-the-buffer-files-that-emacs-creates
 ;;
-;; The autosave files will be located in this directory:
-;;  ~/.emacs_archive/autosaves/
-;; The backup files will be located in this directory:
-;;  ~/.emacs_archive/backups/
-;;
-;; The directories will be automatically created if they do not exist.
+;; Autosave files: ~/.emacs_archive/autosaves/ | Backup files: ~/.emacs_archive/backups/
+;; Directories are automatically created if they don't exist.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Stamp time on all files saved.
 (require 'time-stamp)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Put autosave files (example: #foo#) in ~/.emacs_archive/autosaves/:
+;; Autosave files (example: #foo#) -> ~/.emacs_archive/autosaves/
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defvar autosave-dir (expand-file-name "~/.emacs_archive/autosaves/"))
 (setq auto-save-list-file-prefix autosave-dir)
@@ -34,7 +26,7 @@
 (make-directory autosave-dir t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Put backup files (example: foo~) in ~/.emacs_archive/:
+;; Backup files (example: foo~) -> ~/.emacs_archive/backups/
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defvar backup-dir (expand-file-name "~/.emacs_archive/backups/"))
 (setq backup-directory-alist (list (cons ".*" backup-dir)))
@@ -47,12 +39,9 @@
 (setq backup-by-copying-when-mismatch t)
 (setq backup-by-copying-when-linked t)
 (setq version-control t)
-;; Remove any backups that are either not the 2 oldest copies or the 3
-;; newest copies.
+;; Remove any backups that are either not the 2 oldest copies or the 3 newest copies.
 (setq-default delete-old-versions t)
-;; Keeps at most 3 copies that are newer than the 2 oldest
-;; copies. This means there could be 5 total backups of a file at one
-;; time.
+;; Keeps at most 3 copies that are newer than the 2 oldest copies. This means there could be 5 total backups at one time.
 (setq kept-new-versions 3)
 ;; Keeps two old copies that will not be deleted.
 (setq kept-old-versions 2)
