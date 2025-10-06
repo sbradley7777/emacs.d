eb81e2447e531aab681c865b20514f51a71110f7
Shane Bradley <68629+sbradley7777@users.noreply.github.com>
2025-10-06 16:29:37 -0400

    Fix the show installed packages table to show what is installed in more user friendly manner and include button to update packages with updates.
    Signed-off-by: Shane Bradley <68629+sbradley7777@users.noreply.github.com>

--- core/core-packages.el
+++ core/core-packages.el
@@ -138,11 +138,103 @@ MAX-RETRIES is the maximum number of retry attempts (default: 2)."
  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
  (defun
-  show-installed-packages
-  ()
-  "Show only installed packages in package list."
+  show-installed-packages ()
+  "Show installed packages with clear status labels and update information.
+Packages are labeled as either 'Installed (by User)' or 'Dependency'.
+Shows available version and indicates if updates are available."
   (interactive)
-  (package-show-package-list (mapcar 'car package-alist)))
+  (let ((buf (get-buffer-create "*Installed Packages*"))
+        (packages-with-updates '()))
+
+    ;; Ensure package archive contents are loaded
+    (unless package-archive-contents (package-refresh-contents))
+
+    (with-current-buffer
+     buf (setq buffer-read-only nil) (erase-buffer)
+
+     ;; Collect packages with updates
+     (dolist
+      (pkg (mapcar 'car package-alist))
+      (let* ((installed-desc (cadr (assq pkg package-alist)))
+             (installed-version (package-desc-version installed-desc))
+             (available-desc (cadr (assq pkg package-archive-contents)))
+             (available-version (when available-desc (package-desc-version available-desc))))
+        (when
+         (and available-version (version-list-< installed-version available-version))
+         (message
+          "There is an update for package %s. The current version is \"%s\" and updated version is available \"%s\""
+          pkg
+          (package-version-join installed-version)
+          (package-version-join available-version))
+         (push pkg packages-with-updates))))
+
+     ;; Show update button if updates are available
+     (when
+      packages-with-updates
+      (insert
+       (format
+        "%d package update%s available. "
+        (length packages-with-updates)
+        (if (> (length packages-with-updates) 1) "s" "")))
+      (insert-button
+       "[Update All]" 'action
+       (lambda
+        (_)
+        (when
+         (yes-or-no-p
+          (format
+           "Update %d package%s? "
+           (length packages-with-updates)
+           (if (> (length packages-with-updates) 1) "s" "")))
+         ;; Upgrade packages
+         (dolist (pkg packages-with-updates) (package-upgrade pkg))
+         ;; Reload package state (suppress activation warnings)
+         (let ((inhibit-message t))
+           (package-initialize))
+         (message "Updated %d packages" (length packages-with-updates))
+         (sit-for 1)
+         (show-installed-packages))))
+      (insert "\n\n"))
+
+     ;; Insert table header
+     (insert
+      (format
+       "%-40s %-20s %-18s %-22s %s\n"
+       "Package"
+       "Installed"
+       "Update Available"
+       "Status"
+       "Description"))
+     (insert (make-string 127 ?-) "\n")
+
+     ;; Insert package list
+     (dolist
+      (pkg (sort (mapcar 'car package-alist) 'string<))
+      (let* ((desc (cadr (assq pkg package-alist)))
+             (installed-version (package-desc-version desc))
+             (available-desc (cadr (assq pkg package-archive-contents)))
+             (available-version (when available-desc (package-desc-version available-desc)))
+             (summary (package-desc-summary desc))
+             (status (if (memq pkg package-selected-packages) "Installed (by User)" "Dependency"))
+             (update-str
+              (cond
+               ((not available-version)
+                "N/A")
+               ((version-list-< installed-version available-version)
+                "*")
+               (t
+                ""))))
+        (insert
+         (format
+          "%-40s %-20s %-18s %-22s %s\n"
+          (symbol-name pkg)
+          (package-version-join installed-version)
+          update-str
+          status
+          (or summary "")))))
+
+     (goto-char (point-min)) (setq buffer-read-only t) (help-mode))
+    (switch-to-buffer buf)))
 
  (defun
   search-packages () "Search for packages by name or keyword." (interactive)
