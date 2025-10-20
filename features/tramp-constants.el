51da9d0ff78b02e3f8d37b13f5ca77f6e8425d2b
Shane Bradley <68629+sbradley7777@users.noreply.github.com>
2025-10-20 10:13:54 -0400

    Add new file for tramp.
    Signed-off-by: Shane Bradley <68629+sbradley7777@users.noreply.github.com>

---
+++ features/tramp-constants.el
@@ -0,0 +1,25 @@
+;;; tramp-constants.el --- TRAMP Configuration Constants -*- lexical-binding: t -*-
+
+;;; Commentary:
+;; Constants for TRAMP (Transparent Remote Access, Multiple Protocol) configuration.
+;; These settings control remote file access behavior.
+
+;;; Code:
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; TRAMP (Remote Access) Constants
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defconst
+ tramp-default-shell "/bin/bash"
+ "Default remote shell for TRAMP connections.
+Bash is widely available and provides consistent behavior across remote systems.")
+
+(defconst
+ tramp-user-paths '("~/.local/bin")
+ "Additional remote paths for TRAMP executable search.
+These directories are added to the remote PATH when connecting via TRAMP.")
+
+(provide 'tramp-constants)
+
+;;; tramp-constants.el ends here
