cf1d890ad30487f393c90cf6736fb7d033a6438e
Shane Bradley <68629+sbradley7777@users.noreply.github.com>
2025-11-28 18:13:24 -0500

    Fix incorrect binary.
    Signed-off-by: Shane Bradley <68629+sbradley7777@users.noreply.github.com>

--- lang/markdown/markdown-config.el
+++ lang/markdown/markdown-config.el
@@ -26,7 +26,7 @@
  (setq markdown-fontify-code-blocks-natively t)
  (visual-line-mode 1)
  (setq fill-column core-fill-column)
- (flymake-lang-setup-dual-backend "mdl" 'flymake-collection-markdownlint))
+ (flymake-lang-setup-dual-backend "markdownlint" 'flymake-collection-markdownlint))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; LSP Configuration
@@ -68,6 +68,6 @@
 (lang-register-dual-mode-hooks markdown markdown-setup-common)
 
 (core-message-lang-loaded
- "Markdown" "markdown-mode and markdown-ts-mode with marksman LSP and mdl linter")
+ "Markdown" "markdown-mode and markdown-ts-mode with marksman LSP and markdownlint linter")
 (provide 'markdown-config)
 ;;; markdown-config.el ends here
